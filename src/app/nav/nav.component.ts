7fa275d3ee0028fec05bd4a97cdca75f1a4f7cb5
anowakowski <nowakowski.add@gmail.com>
2018-11-06 20:59:44 +0100

    added nagular forms

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loggedIn() {
+    return true;
+  }
+
+  login(): void {
+
   }
 
 }
