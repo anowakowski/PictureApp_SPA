e25a18bbf57f1bb788200504b181334a10fcb4ab
anowakowski <nowakowski.add@gmail.com>
2018-11-07 21:17:52 +0100

    add http client to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
    declarations: [
@@ -14,9 +16,12 @@ import { NavComponent } from './nav/nav.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      FormsModule
+      FormsModule,
+      HttpClientModule
    ],
-   providers: [],
+   providers: [
+       AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
