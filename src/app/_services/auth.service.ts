11b375803b146af422772ba0f1602c67910c1631
anowakowski <nowakowski.add@gmail.com>
2018-11-07 21:16:14 +0100

    added auth services, prepare apiUrl in environment

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.apiUrl;
+  jwtHelper = new JwtHelperService();
+
+  constructor(private http: HttpClient) { }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
+}
