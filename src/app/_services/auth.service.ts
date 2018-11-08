110b5f02772616c4bdeb6747d260a0ea108a9dcd
anowakowski <nowakowski.add@gmail.com>
2018-11-08 23:08:44 +0100

    fixed stylecope

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
 
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
