0bb6a0b6efa32786f94fc65b7498e2e58916cd19
anowakowski <nowakowski.add@gmail.com>
2018-12-13 23:22:08 +0100

    added home component

--- src/app/pictureapp-exploring/pictureapp-exploring.module.ts
+++ src/app/pictureapp-exploring/pictureapp-exploring.module.ts
@@ -6,11 +6,13 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { MaterialModule } from '../shared/material/material.module';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path: '', component: PictureappExploringComponent,
     children: [
-      {path: '', component: MainContentComponent}
+      {path: '', component: HomeComponent},
+      {path: 'main-content', component: MainContentComponent}
     ]},
   {path: '**', redirectTo: ''}
 ];
@@ -20,7 +22,8 @@ const routes: Routes = [
     PictureappExploringComponent,
     SidenavComponent,
     ToolbarComponent,
-    MainContentComponent
+    MainContentComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
