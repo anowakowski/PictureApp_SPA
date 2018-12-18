543785281e3337af22467a0bf79659786e9f520a
anowakowski <nowakowski.add@gmail.com>
2018-12-18 10:58:39 +0100

    fixed pullRequests(#21) comments

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/_services/auth.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { MatSnackBar } from '@angular/material';
+import { SnacbarAlertService } from 'src/app/_services/snacbar-alert.service';
 
 @Component({
   selector: 'app-register-form',
@@ -17,7 +19,8 @@ export class RegisterFormComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private formBuilder: FormBuilder,
-    private router: Router) { }
+    private router: Router,
+    private snacbarService: SnacbarAlertService) { }
 
   ngOnInit() {
     this.createRegisterForm();
@@ -27,9 +30,9 @@ export class RegisterFormComponent implements OnInit {
     if (this.registerForm.valid) {
       this.user = Object.assign({}, this.registerForm.value);
       this.authService.register(this.user).subscribe(() => {
-        // this.alertifyService.success('registration succesful');
+        this.snacbarService.openSnackbar('register successfully', 5000);
       }, error => {
-        // this.alertifyService.error(error);
+        this.snacbarService.openSnackbar(error, 7000, null, 'orange-register-login-snackbar');
       }, () => {
         this.authService.login(this.user).subscribe(() => {
           this.router.navigate(['/explore']);
