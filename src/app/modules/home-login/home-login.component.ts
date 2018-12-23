b3c787201cda961cb3f6625fb634ca0fc345c4de
anowakowski <nowakowski.add@gmail.com>
2018-12-23 13:21:08 +0100

    remove unneccesery login

--- src/app/modules/home-login/home-login.component.ts
+++ src/app/modules/home-login/home-login.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `
+  <div class="body">
+    <app-nav></app-nav>
+    <div class="container"><router-outlet></router-outlet></div>
+  </div>
+`,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
