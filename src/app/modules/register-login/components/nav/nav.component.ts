9f7c5dbd1cb22f5c82c61fcde1ffcbc520c3854f
anowakowski <nowakowski.add@gmail.com>
2018-12-31 16:41:42 +0100

    prepare go to home from navbar

--- src/app/modules/register-login/components/nav/nav.component.ts
+++ src/app/modules/register-login/components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { SearchPicturesDialogComponent } from '../search-pictures-dialog/search-pictures-dialog.component';
 import { DialogService } from '../../../services/dialog.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +11,16 @@ import { DialogService } from '../../../services/dialog.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private dialog: MatDialog, private dialogService: DialogService) { }
+  constructor(private dialog: MatDialog, private dialogService: DialogService, private router: Router) { }
 
   ngOnInit() {
   }
+
   openSearchDialog(): void {
     this.dialogService.openDialog(SearchPicturesDialogComponent, null, '150px', {top: '100px'});
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']);
   }
 }
