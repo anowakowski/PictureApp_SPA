39d5cb7fd8b6ce570a3c26a26ac4de4a9062cea7
anowakowski <nowakowski.add@gmail.com>
2018-12-31 15:48:49 +0100

    prepare more register inputs with validations

--- src/app/modules/register-login/components/register/register.component.ts
+++ src/app/modules/register-login/components/register/register.component.ts
@@ -6,15 +6,22 @@ import { Router } from '@angular/router';
 import { SnacbarAlertService } from 'src/app/services/snacbar-alert.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 
+const REQUIRES_MESSAGE = 'You must enter a value';
+const NOTVALID_EMAILMESSAGE = 'Not a valid email';
+const BLANK_MESSAGE = '';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
+
   hide = true;
   user: User;
   registerForm: FormGroup;
+
 
   constructor(
     private authService: AuthService,
@@ -34,19 +41,58 @@ export class RegisterComponent implements OnInit {
 
   createRegisterForm() {
     this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-    });
+      confirmPassword: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
   }
 
   getEmailErrorMessage() {
-    return this.registerForm.get('email').hasError('required') ? 'You must enter a value' :
-        this.registerForm.get('email').hasError('email') ? 'Not a valid email' :
+    return this.isRequiredMessage('email') ? REQUIRES_MESSAGE :
+        this.registerForm.get('email').hasError('email') ? NOTVALID_EMAILMESSAGE :
             '';
   }
 
   getPasswordErrorMessage() {
-    return this.registerForm.get('password').hasError('required') ? 'You must enter a value' : '';
+    return this.isRequiredMessage('password') ? REQUIRES_MESSAGE : BLANK_MESSAGE;
+  }
+
+  getConfirmPasswordErrorMessage() {
+    return this.isRequiredMessage('confirmPassword') ? REQUIRES_MESSAGE : BLANK_MESSAGE;
+  }
+
+  getConfirmPasswordMismatchErrorMessage() {
+    return 'mismatch';
+  }
+
+  getUsernameErrorMessage() {
+    return this.isRequiredMessage('username') ? REQUIRES_MESSAGE : BLANK_MESSAGE;
+  }
+
+  isInvalid(fieldName: string) {
+    return this.registerForm.get(fieldName).invalid;
+  }
+
+  onPasswordInput() {
+    if (this.registerForm.hasError('mismatch')) {
+      this.registerForm.get('confirmPassword').setErrors([{'mismatch': true}]);
+    } else {
+      this.registerForm.get('confirmPassword').setErrors(null);
+    }
+  }
+
+  isMismatch(): boolean {
+    // this.registerForm.get('confirmPassword').setErrors([{'mismatch': true}]);
+    return this.registerForm.hasError('mismatch');
+  }
+
+  private isRequiredMessage(fieldName: string): boolean {
+    return this.registerForm.get(fieldName).hasError('required');
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
 }
