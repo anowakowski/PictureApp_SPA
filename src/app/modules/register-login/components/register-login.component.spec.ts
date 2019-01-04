1ca0fbc15276b6fb1db220d585f015b20013f1fb
anowakowski <nowakowski.add@gmail.com>
2019-01-04 15:07:56 +0100

    fixed blank line

--- src/app/modules/register-login/components/register-login.component.spec.ts
+++ src/app/modules/register-login/components/register-login.component.spec.ts
@@ -43,7 +43,6 @@ describe('RegisterLoginComponent', () => {
         .compileComponents();
     }));
 
-
     beforeEach(() => {
         fixture = TestBed.createComponent(RegisterLoginComponent);
         component = fixture.componentInstance;
