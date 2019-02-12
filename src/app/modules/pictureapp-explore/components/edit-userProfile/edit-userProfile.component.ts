8df5beb2c7fb2685c479144bdfa2d475b62445e0
anowakowski <nowakowski.add@gmail.com>
2019-02-12 15:04:29 +0100

    added edit-userProfile component, fixed scss for sidenav

---
+++ src/app/modules/pictureapp-explore/components/edit-userProfile/edit-userProfile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-userProfile',
+  templateUrl: './edit-userProfile.component.html',
+  styleUrls: ['./edit-userProfile.component.scss']
+})
+export class EditUserProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
