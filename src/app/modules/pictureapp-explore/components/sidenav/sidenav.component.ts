728bb72523cff0d75a1438ede2f99033f41b099f
anowakowski <nowakowski.add@gmail.com>
2019-02-12 16:27:43 +0100

    prepare logic for change sidenavbutton colors

--- src/app/modules/pictureapp-explore/components/sidenav/sidenav.component.ts
+++ src/app/modules/pictureapp-explore/components/sidenav/sidenav.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material';
+import { Router } from '@angular/router';
 
 const SMALL_WIDTH_BREAKPOINT = 720;
+const CURRENT_BTN_COLOR = 'warn';
+const BASIC_BTN_COLOR = 'primary';
+const DASHBOARD_SECTION = 'dashboard';
+const USERPROFILE_SECCTION = 'editUserProfile';
 
 @Component({
   selector: 'app-sidenav',
@@ -14,19 +19,62 @@ export class SidenavComponent implements OnInit {
   private mediaMatcher: MediaQueryList =
     matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
 
-  constructor() { }
+  constructor(public router: Router) { }
+
+  public currentRouteSecction: string;
+  public currentChosenSection: string;
+
+  public currentColor = CURRENT_BTN_COLOR;
+  public basicColor = BASIC_BTN_COLOR;
+  public dashboardSection = DASHBOARD_SECTION;
+  public userprofileSection = USERPROFILE_SECCTION;
+
+  public dashboardColor = CURRENT_BTN_COLOR;
+  public userProfileColor = BASIC_BTN_COLOR;
 
   @ViewChild(MatDrawer) drawer: MatDrawer;
 
   ngOnInit() {
+    this.currentChosenSection = DASHBOARD_SECTION;
+    this.currentRouteSecction = this.router.url;
+    this.setCurrentSectionByRoute();
   }
 
   isScreenSmall(): boolean {
     return this.mediaMatcher.matches;
   }
 
-  goToUserProfile() {
+  setBtnColor(sectionNameToCurrent: string) {
+    if (sectionNameToCurrent === this.currentChosenSection) {
+      return;
+    }
+    this.setCurrentColor(sectionNameToCurrent);
+    this.setBasicColor();
 
+    this.currentChosenSection = sectionNameToCurrent;
   }
 
+  private setBasicColor() {
+    if (this.currentChosenSection === DASHBOARD_SECTION) {
+      this.dashboardColor = BASIC_BTN_COLOR;
+    } else if (this.currentChosenSection === USERPROFILE_SECCTION) {
+      this.userProfileColor = BASIC_BTN_COLOR;
+    }
+  }
+
+  private setCurrentColor(sectionNameToCurrent: string) {
+    if (sectionNameToCurrent === DASHBOARD_SECTION) {
+      this.dashboardColor = CURRENT_BTN_COLOR;
+    } else if (sectionNameToCurrent === USERPROFILE_SECCTION) {
+      this.userProfileColor = CURRENT_BTN_COLOR;
+    }
+  }
+
+  private setCurrentSectionByRoute() {
+    if (this.currentRouteSecction.search(USERPROFILE_SECCTION) === 1) {
+      this.setBtnColor(USERPROFILE_SECCTION);
+      return;
+    }
+    this.setBtnColor(DASHBOARD_SECTION);
+  }
 }
