c5edf8be5600d8c5a38a55817e02b35ec52d65f1
anowakowski <nowakowski.add@gmail.com>
2019-02-19 13:27:12 +0100

    prepare more colorfull chart

--- src/app/modules/pictureapp-explore/components/sidenav/components/sidenav-userprofile-chart/sidenav-userprofile-chart.component.ts
+++ src/app/modules/pictureapp-explore/components/sidenav/components/sidenav-userprofile-chart/sidenav-userprofile-chart.component.ts
@@ -11,9 +11,9 @@ export class SidenavUserprofileChartComponent implements OnInit {
 
   // lineChart
   public lineChartData: Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Photos'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'New followers'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Your likes'}
   ];
   public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: any = {
@@ -22,25 +22,25 @@ export class SidenavUserprofileChartComponent implements OnInit {
   public lineChartType = 'line';
   public lineChartLegend = true;
   public lineChartColors: Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
+    { // photos
+      backgroundColor: 'rgba(102,0,204,0.2)',
+      borderColor: 'rgba(102,0,204, 1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
+    { // new followers
+      backgroundColor: 'rgba(255,153,0,0.2)',
+      borderColor: 'rgba(255,153,0,1)',
       pointBackgroundColor: 'rgba(77,83,96,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(77,83,96,1)'
     },
     { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
+      backgroundColor: 'rgba(255,51,102,0.2)',
+      borderColor: 'rgba(255,51,102,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
