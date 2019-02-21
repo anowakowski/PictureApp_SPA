22b751c56de892b23b7941b4c2de396b16846477
anowakowski <nowakowski.add@gmail.com>
2019-02-21 12:33:35 +0100

    prepare more effective user profile picture

--- src/app/modules/pictureapp-explore/components/edit-userProfile/edit-userProfile.component.ts
+++ src/app/modules/pictureapp-explore/components/edit-userProfile/edit-userProfile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Photo } from 'src/app/models/photo';
 
 @Component({
   selector: 'app-edit-user-profile',
@@ -11,15 +12,26 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class EditUserProfileComponent implements OnInit {
 
   user: User;
+  mainPhoto: Photo;
+  userPhotos: Photo[];
   constructor(private route: ActivatedRoute, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     this.spinner.show();
     this.route.data.subscribe(data => {
       this.user = data['user'];
+      this.setMainPhoto();
+      this.setListOfPhotosWithoutMainPicture();
       this.spinner.hide();
     }, error => {
     });
   }
 
+  setMainPhoto() {
+    this.mainPhoto = this.user.photos.find(p => p.isMain);
+  }
+
+  setListOfPhotosWithoutMainPicture() {
+    this.userPhotos = this.user.photos.filter(p => !p.isMain);
+  }
 }
