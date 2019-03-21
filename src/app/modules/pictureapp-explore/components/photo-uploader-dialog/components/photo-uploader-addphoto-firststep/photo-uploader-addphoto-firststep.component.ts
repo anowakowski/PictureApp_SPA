8826d598cbf21f8242b06280d2fb568429d34ec1
Adrian Nowakowski <adriannowakowski@MacBook-Pro-Adrian.local>
2019-03-21 11:25:57 +0100

    prepare image preview

--- src/app/modules/pictureapp-explore/components/photo-uploader-dialog/components/photo-uploader-addphoto-firststep/photo-uploader-addphoto-firststep.component.ts
+++ src/app/modules/pictureapp-explore/components/photo-uploader-dialog/components/photo-uploader-addphoto-firststep/photo-uploader-addphoto-firststep.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from 'src/environments/environment';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-photo-uploader-addphoto-firststep',
@@ -12,12 +13,15 @@ export class PhotoUploaderAddphotoFirststepComponent implements OnInit {
   @Input() fileInput: File;
 
   public uploader: FileUploader;
+  public filePreviewPath: SafeUrl;
+
   baseUrl = environment.apiUrl;
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.initUploader();
     this.setCurrentPhotoForUploader();
+    this.prepareImagePreview();
   }
 
   initUploader() {
@@ -32,6 +36,12 @@ export class PhotoUploaderAddphotoFirststepComponent implements OnInit {
 
   setCurrentPhotoForUploader() {
     this.uploader.addToQueue(this.getCurrentFiles());
+  }
+
+  prepareImagePreview () {
+
+    const item = this.uploader.queue[0];
+    this.filePreviewPath  = this.sanitizer.bypassSecurityTrustUrl((window.URL.createObjectURL(item._file)));
   }
 
   private getCurrentFiles() {
