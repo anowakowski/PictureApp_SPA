ca9b0b40a499b5e7a2f0b708c43444738c98c3f1
Adrian Nowakowski <adriannowakowski@MacBook-Pro-Adrian.local>
2019-03-25 21:02:31 +0100

    prepare nav and sidenav components

--- src/app/modules/photo-uploader/photo-uploader.module.ts
+++ src/app/modules/photo-uploader/photo-uploader.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoUploaderComponent } from './photo-uploader.component';
+import { MaterialModule } from '../shared/material/material.module';
+import { NavComponent } from './components/nav/nav.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  imports: [
-    CommonModule
+  declarations: [
+    PhotoUploaderComponent,
+    NavComponent
   ],
-  declarations: [PhotoUploaderComponent]
+  imports: [
+    CommonModule,
+    MaterialModule,
+    SidenavComponent
+  ]
 })
 export class PhotoUploaderModule { }
